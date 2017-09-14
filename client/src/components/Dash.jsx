c0e2b0684658fb15242fb8cdddb1526172ff8ecd
Asher Shaheen <ashershaheen@gmail.com>
2017-09-14 10:12:21 -0400

    added dash component to check the auth for user and render imageupload

---
+++ client/src/components/Dash.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Auth from '../modules/Auth';
+import ImageUpload from './ImageUpload';
+
+class Dash extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: null,
+      userDataLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    this.props.resetFireRedirect()
+    axios('/profile', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Token ${Auth.getToken()}`,
+        token: `${Auth.getToken()}`,
+      }
+    }).then(res => {
+      console.log(res.data.user)
+      this.setState({
+        userData: res.data.user,
+        userDataLoaded: true,
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="dash">
+        {(this.state.userDataLoaded) ? <ImageUpload /> : <p>Loading ...</p>}
+      </div>
+    );
+  }
+}
+
+export default Dash;
