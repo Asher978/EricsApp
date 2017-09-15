368b6c52b665aecac0fdfad0f112c8df93015fe0
Asher Shaheen <ashershaheen@gmail.com>
2017-09-15 10:20:20 -0400

    updated schema to reflect upload migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913162809) do
+ActiveRecord::Schema.define(version: 20170915135949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "uploads", force: :cascade do |t|
+    t.string "pic"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
