8da49bc988c9e47647d684a7662ad6a2f6258b99
Asher Shaheen <ashershaheen@gmail.com>
2017-09-15 19:53:37 -0400

    schema was updated as a resut of amigrations for appointments

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170915135949) do
+ActiveRecord::Schema.define(version: 20170915202609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.date "date"
+    t.time "time"
+    t.text "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
 
   create_table "uploads", force: :cascade do |t|
     t.string "pic"
