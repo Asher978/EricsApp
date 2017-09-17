5b71da9c2bd5bcf5aaa54b0c60e5fdddfedfdc9e
Asher Shaheen <ashershaheen@gmail.com>
2017-09-17 18:42:53 -0400

    added the admin params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,11 @@ class UsersController < ApiController
 
     def profile
         user = User.find_by_auth_token!(request.headers[:token])
-        render json: { user: { username: user.username, email: user.email, firstname: user.firstname, lastname: user.lastname } }
+        render json: { user: { username: user.username, email: user.email, firstname: user.firstname, lastname: user.lastname, admin: user.admin } }
     end
 
     private
     def user_params
-        params.require(:user).permit(:username, :email, :password, :name)
+        params.require(:user).permit(:username, :email, :password, :firstname, :lastname, :admin)
     end
 end
