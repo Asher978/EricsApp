4f2b4327242654c15779ee1ec8fd2ae847d47499
Asher Shaheen <ashershaheen@gmail.com>
2017-09-17 18:48:35 -0400

    added method send_signup_email

---
+++ app/mailers/appointment_notifier_mailer.rb
@@ -0,0 +1,10 @@
+class AppointmentNotifierMailer < ApplicationMailer
+    default :from => 'ashershaheen@gmail.com
+    '
+    def send_signup_email(user, appointment)
+        @user = user
+        @appointment = appointment
+        mail( :to => 'ashershaheen@gmail.com',
+        :subject => 'Thanks for booking an appointment!' )
+    end
+end
