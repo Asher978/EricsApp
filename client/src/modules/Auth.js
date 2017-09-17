af5adae39bb8b346cabbff80b5dbcd7a02f912c9
Asher Shaheen <ashershaheen@gmail.com>
2017-09-17 14:17:51 -0400

    added methods to save admin status in sessionstorage

--- client/src/modules/Auth.js
+++ client/src/modules/Auth.js
@@ -1,19 +1,33 @@
 class Auth {
     // Source - https://vladimirponomarev.com/blog/authentication-in-react-apps-jwt
-    static authenticateToken(token) {
+    static authenticateToken(token, admin) {
       sessionStorage.setItem('token', token);
+      sessionStorage.setItem('admin', admin);      
     }
   
     static isUserAuthenticated() {
       return sessionStorage.getItem('token') !== null;
     }
+
+    static isUserAdmin() {
+       if (sessionStorage.getItem('admin') !== null) {
+         return sessionStorage.getItem('admin')
+       } else {
+         return false
+       }
+    }
   
     static deauthenticateUser() {
       sessionStorage.removeItem('token');
+      sessionStorage.removeItem('admin');      
     }
   
     static getToken() {
       return sessionStorage.getItem('token');
+    }
+
+    static getAdmin() {
+      return sessionStorage.getItem('admin');
     }
   
   }
