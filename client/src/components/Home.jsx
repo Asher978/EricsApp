f4eac14d3cbd6aaa889e89bbe68c3447081736e4
Asher Shaheen <ashershaheen@gmail.com>
2017-09-20 08:42:03 -0400

    added home page pics and content

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import imageA from '../assets/imageA.jpg'
 import imageB from '../assets/imageB.jpg'
 import imageC from '../assets/imageC.jpg'
+import wedding from '../assets/wedding.jpg'
+import days from '../assets/days.jpg'
+import kids from '../assets/kids.jpg'
+import camera from '../assets/camera.jpg'
+import birthday from '../assets/birthday.jpg'
+import rings from '../assets/rings.png'
+import privateA from '../assets/privateA.jpg'
+
 
 const Home = () => {
   return (
@@ -18,19 +26,19 @@ const Home = () => {
         <img className="first-slide" src={imageA} alt="First slide" />
         <div className="container">
           <div className="carousel-caption">
-            <h1>Example headline.</h1>
-            <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
-            <p><a className="btn btn-lg btn-primary" href="" role="button">Sign up today</a></p>
+            <h1>-----I HEART NEW YORK-----</h1>
+            <h2>STYLISH PHOTO SHOOTS FEATURING YOU IN THE GREATEST CITY IN THE WORLD</h2>
+            <p><a className="btn btn-lg btn-primary" href="/register" role="button">Sign up today</a></p>
           </div>
         </div>
       </div>
       <div className="item">
-        <img className="second-slide" src={imageB} alt="Second slide" />
+        <img className="second-slide" src={camera} alt="Second slide" />
         <div className="container">
           <div className="carousel-caption">
-            <h1>Another example headline.</h1>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a className="btn btn-lg btn-primary" href="" role="button">Learn more</a></p>
+          <h1>-----I HEART NEW YORK-----</h1>
+            <h2>STYLISH PHOTO SHOOTS FEATURING YOU IN THE GREATEST CITY IN THE WORLD</h2>
+            <p><a className="btn btn-lg btn-primary" href="/bio" role="button">View My Gallery</a></p>
           </div>
         </div>
       </div>
@@ -38,9 +46,9 @@ const Home = () => {
         <img className="third-slide" src={imageC} alt="Third slide" />
         <div className="container">
           <div className="carousel-caption">
-            <h1>One more for good measure.</h1>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a className="btn btn-lg btn-primary" href="" role="button">Browse gallery</a></p>
+          <h1>-----I HEART NEW YORK-----</h1>
+            <h2>STYLISH PHOTO SHOOTS FEATURING YOU IN THE GREATEST CITY IN THE WORLD</h2>
+            <p><a className="btn btn-lg btn-primary" href="/calendar" role="button">View My Calendar</a></p>
           </div>
         </div>
       </div>
@@ -58,22 +66,19 @@ const Home = () => {
   <div className="container marketing">
     <div className="row">
       <div className="col-lg-4">
-        <img className="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder" width="140" height="140" />
+        <img className="img-circle" src={birthday} alt="Generic placeholder" width="140" height="140" />
         <h2>Bithdays</h2>
-        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-        <p><a className="btn btn-default" href="" role="button">View details &raquo;</a></p>
+        <p>Birthday party packages begin at $550 for ninety minutes of coverage and include full resolution images on a custom USB drive. Please click on the “Register” tab above and schedule your event! Please note, I am now booking parties for October 2017 and later.</p>
       </div>
       <div className="col-lg-4">
-        <img className="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder" width="140" height="140" />
+        <img className="img-circle" src={rings} alt="Generic placeholder" width="140" height="140" />
         <h2>Weddings</h2>
-        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-        <p><a className="btn btn-default" href="" role="button">View details &raquo;</a></p>
+        <p>I have been given an amazing gift: I know what beautiful looks like... And I know how to capture it for you. To give you your most precious memories...</p>
       </div>
       <div className="col-lg-4">
-        <img className="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder" width="140" height="140" />
+        <img className="img-circle" src={privateA} alt="Generic placeholder" width="140" height="140" />
         <h2>Private Events</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a className="btn btn-default" href="" role="button">View details &raquo;</a></p>
+        <p>If you are looking for a photographer for Private or Corporate events, you've come to the right place! Book a phone consultation by registering on this site. I look forward to speaking with you.</p>
       </div>
     </div>
 
@@ -83,10 +88,10 @@ const Home = () => {
     <div className="row featurette">
       <div className="col-md-7">
         <h2 className="featurette-heading">I Love it all. <span className="text-muted">the beautiful, the memorable, the downright weird</span></h2>
-        <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        
       </div>
       <div className="col-md-5">
-        <img className="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder" />
+        <img className="featurette-image img-responsive center-block" src={wedding} alt="Generic placeholder" />
       </div>
     </div>
 
@@ -95,10 +100,10 @@ const Home = () => {
     <div className="row featurette">
       <div className="col-md-7 col-md-push-5">
         <h2 className="featurette-heading">Because you're awesome even on days you're not getting married. <span className="text-muted">(And probably calmer.)</span></h2>
-        <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        
       </div>
       <div className="col-md-5 col-md-pull-7">
-        <img className="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder" />
+        <img className="featurette-image img-responsive center-block" src={days} alt="Generic placeholder" />
       </div>
     </div>
 
@@ -107,10 +112,10 @@ const Home = () => {
     <div className="row featurette">
       <div className="col-md-7">
         <h2 className="featurette-heading">You look great! <span className="text-muted">Let's also show your future grandkids how much fun you had.</span></h2>
-        <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+       
       </div>
       <div className="col-md-5">
-        <img className="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder" />
+        <img className="featurette-image img-responsive center-block" src={kids} />
       </div>
     </div>
 
