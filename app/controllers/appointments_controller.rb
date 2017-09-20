f75f4cfefa731e2365a0cb78a8506bef57b5d336
Asher Shaheen <ashershaheen@gmail.com>
2017-09-19 23:00:09 -0400

    added method to delete a booking

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -10,14 +10,22 @@ class AppointmentsController < ApiController
         appointment = Appointment.new(appointment_params)
         appointment.user = current_user
         if appointment.save
+            event = Appointment.all
             AppointmentNotifierMailer.send_signup_email(appointment.user, appointment).deliver
             render json: {
                 message: 'Appointment was created and An email was sent to Eric! He will be in touch shortly. Thank you!',
                 appointment: appointment,
+                event: event.map(&:serialize)
             }
         else
             render json: { message: 'Error in the submission. Please submit your request again!' }
         end
+    end
+
+    def destroy
+        app = Appointment.delete(params[:appointment_id])
+        appointments = Appointment.all
+        render json: { event: appointments.map(&:serialize) }
     end
 
     private
