485f9590ef80f9da34de5950f3f3cb521c63b303
Asher Shaheen <ashershaheen@gmail.com>
2017-09-19 22:52:10 -0400

    able to show all bookings on a calendar

---
+++ client/src/components/Calendar.jsx
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import { Jumbotron } from 'react-bootstrap';
+import { Alert } from 'reactstrap';
+
+require('react-big-calendar/lib/css/react-big-calendar.css');
+
+
+BigCalendar.setLocalizer( BigCalendar.momentLocalizer(moment) );
+
+
+class Calendar extends Component {
+  constructor () {
+    super();
+    this.state = {
+      appoint: '',
+      appointmentsLoaded: false,
+      visible: false,
+      event: null,
+      title: null,
+
+    }
+  }
+  componentDidMount() {
+    axios('/appointments', {
+      method: 'GET',
+    }).then(res => {
+      console.log(res)
+      let e = res.data.event.map(eve => {
+        return {
+          title: eve.username,
+          start: new Date(eve.date),
+          end: moment(eve.date).add('minutes', 20),
+        }  
+      });
+      this.setState({
+        event: e,    
+        title: e.title,  
+        appointmentsLoaded: true,
+      })
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  renderCalendar () {
+    return (this.state.appointmentsLoaded) ? 
+      <BigCalendar
+        style={{height: '420px'}}
+        events={this.state.event}
+        onSelectEvent={(eve) => this.renderEventPopUp(eve)}
+        /> : (
+        <div>Loading....</div>
+      )
+  }
+
+  renderEventPopUp = (event) => {
+    this.setState({ visible: true, appoint: event.title })  
+  }
+
+  onDismiss = () => {
+    this.setState({ visible: false });
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <Jumbotron>
+          <h1>Current Bookings</h1>
+          <p>Below are my current bookings. Please schedule your consultations with me accordingly!</p>
+        </Jumbotron>
+        {this.renderCalendar()}
+        <Alert color="info" isOpen={this.state.visible} toggle={this.onDismiss}>
+          {this.state.appoint}
+        </Alert>
+        <footer>
+          <p className="pull-right"></p>
+        <p>&copy; Wiley Media & Inc. &middot; <a href="">Privacy</a></p>
+        </footer>
+      </div>
+    )
+  }
+}
+
+
+export default Calendar;
