88055e99216163a2bbdb2161d2d02275a07fff3d
Asher Shaheen <ashershaheen@gmail.com>
2017-09-21 22:33:29 -0400

    delete feature completed for bookings with firebase and db

--- client/src/components/AdminBookings.jsx
+++ client/src/components/AdminBookings.jsx
@@ -1,30 +1,70 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Jumbotron } from 'react-bootstrap';
 import moment from 'moment';
 
 
+
 class AdminBookings extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            appointments: this.props.appointments,
+            appointmentsLoaded: this.props.appointmentsLoaded,
+        }
+
+    }
+    
+
 
     renderClientsBookings = () => {
         if (this.props.appointmentsLoaded) {
             return this.props.appointments.map(book => {
                 return (
-                    <tr key={book.id}>
-                        <td>{book.username}&nbsp;<button type="button"
+                    <tr key={book.id} id={book.key}>
+                        <td>{book.user}&nbsp;<button type="button"
                                                          className="btn btn-danger btn-xs" 
                                                          aria-label="Left Align"
-                                                        onClick={() => this.props.handleDeleteBooking(book.id)}>
-                                                        <span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
-                                                        </button>
-                                                        </td>
+                                                         onClick={() => this.props.handleDeleteBooking(book.id, book.key)}>
+                                                         <span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                                                         </button>
+                                                         </td>
                         <td>{book.date}</td>
-                        <td>{moment(book.time).format("hh:mm A")}</td>
+                        <td>{`${moment(book.time, 'HH:mm').format('hh:mm a')}`}</td>
                     </tr>
                 )
             })
         }
     }
+
+    // componentWillReceiveProps(nextProps) {
+    //     console.log('props recevied', nextProps)
+    //     this.setState({
+    //         appointments: nextProps.appointments,
+    //         appointmentsLoaded: nextProps.appointmentsLoaded,
+    //     })
+    // }
+
+    // componentDidMount() {
+    //     console.log('from adminBookings')
+    //     this.props.getAppointments()
+    // }
+
+    // componentWillMount () {
+    //     const savedBokoking = this.state.appointments;
+    //     // DataSnapshot
+    //     this.rootRef.on('child_added', snap => {
+    //       savedZ.push({
+    //         id: snap.key,
+    //         zipContent: snap.val().zipContent,
+    //         weatherContent: snap.val().weatherContent,
+    //         dateContent: snap.val().dateContent,
+    //       })
+    //       this.setState({
+    //         fbZip: savedZ,
+    //       })
+    //     })
+    // }
+
 
     render () {
         return (
